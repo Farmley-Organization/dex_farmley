c200b8465ecb66817ba94af890a788a7e7902249
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-02-03 15:09:19 +0530

    franchize partner

--- dex_farmley/dexfarmley/custom_purchase_receipt.py
+++ dex_farmley/dexfarmley/custom_purchase_receipt.py
@@ -7,4 +7,6 @@ import frappe
 @frappe.whitelist()
 def make_sales_return(source_name, target_doc=None):
     from erpnext.controllers.sales_and_purchase_return import make_return_doc
-    return make_return_doc("Delivery Note", source_name, target_doc)
+    doc=make_return_doc("Delivery Note", source_name, target_doc)
+    doc.save(ignore_permissions=True)
+    doc.submit()
