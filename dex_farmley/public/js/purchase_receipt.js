6e5997c4070e961560079d7844ebc6419cdf2220
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-02-07 17:53:04 +0530

    changes

--- dex_farmley/public/js/purchase_receipt.js
+++ dex_farmley/public/js/purchase_receipt.js
@@ -1,37 +1,21 @@
 frappe.ui.form.on("Purchase Receipt", {
-    // on_submit:function(frm){
-    //     if(frm.doc.is_return){
-    //     frappe.model.get_value('Purchase Receipt', {"return_against":frm.doc.return_against}, 'inter_company_reference',
-  	// 	function(d) {
-    //     frappe.call({
-    //         method:"dex_farmley.dexfarmley.custom_purchase_receipt.make_sales_return",
-    //         args: {"source_doc":d.inter_company_reference},
-    //         callback: function(r){
-                
-    //         },
-    //     })
-    // })
-    // }
-
-    // }
-refresh:function(frm){
-    if (frm.doc.docstatus==1 && frm.doc.is_return==1) {
-        frappe.model.get_value('Purchase Receipt', {"return_against":frm.doc.return_against}, 'inter_company_reference',
-            function(d) {
-                console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$",d.inter_company_reference)
-            frm.add_custom_button(__('Sales Return'), function() {
-                frappe.call({
-                    method:"dex_farmley.dexfarmley.custom_purchase_receipt.make_sales_return",
-                    args: {
-                        source_name:d.inter_company_reference,
-                    },
-                    callback: function(r){
-                        
-                    },
-                })
-                    }, __('Create'));
-        })
+    on_submit:function(frm){
+        if (frm.doc.is_return==1) {
+            frappe.model.get_value('Purchase Receipt', frm.doc.return_against, 'inter_company_reference',
+                function(d) {
+                    frappe.call({
+                        method:"dex_farmley.dexfarmley.custom_purchase_receipt.make_sales_return",
+                        args: {
+                            source_name:d.inter_company_reference,
+                            name:frm.doc.name
+                        },
+                        callback: function(r){
+                            
+                        },
+                    })
+                    
+            })
+        }
     }
-}
 })
 
